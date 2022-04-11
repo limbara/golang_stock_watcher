e51fa8ce628d4afdd9e4e4e2fce9723fad826e47
Nico <limbaranico@yahoo.com>
2022-04-11 14:35:21 +0700

    fix heroku exec

--- heroku-exec.sh
+++ heroku-exec.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 # https://devcenter.heroku.com/articles/exec#using-with-docker
+# https://gist.github.com/wwerner/05a8e627e8f3ba18300db745511d3bcb
+set +o posix
+
 [ -z "$SSH_CLIENT" ] && source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL")
